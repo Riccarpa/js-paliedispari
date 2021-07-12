714d4771379d1112b21232b4cc9fb0bac56dc954
Riccarpa <riccardopagni99@gmail.com>
2021-07-12 18:10:46 +0200

    palindromi done

--- js/palindromi.js
+++ js/palindromi.js
@@ -3,17 +3,27 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-
-var userWord = prompt('inserisci una parola', 'anna');
-userWord = stringRevert(userWord);
+// 
+var userWord = prompt('inserisci una parola', 'anna').trim();
+userWordReverse = stringRevert(userWord);
+console.log(userWord);
+console.log(userWordReverse);
 
 function stringRevert(word) {
     var word = word.split('');
     console.log(word);
     var wordReverse = ' ';
-    for (i = userWord.length; i >= 0; i--) {
+    for (i = userWord.length - 1; i >= 0; i--) {
+
         wordReverse += word[i];
     }
-    console.log(wordReverse);
 
+    return wordReverse.trim();
+
+}
+
+if (userWordReverse === userWord) {
+    console.log('palindrome');
+} else {
+    console.log('non palindrome');
 }
