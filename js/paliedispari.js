d1881c0f0176e1305bf7e7957ef4d2d776803df4
Riccarpa <riccardopagni99@gmail.com>
2021-07-12 15:40:17 +0200

    preparazione html e inizio js

---
+++ js/paliedispari.js
@@ -0,0 +1,29 @@
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//link to html
+
+var evenOdd = document.getElementById('even-odd');
+var userNumber = document.getElementById('user-number');
+var button = document.getElementById('button');
+var finalResult = document.getElementById('result');
+
+
+
+
+// attivare il bottone e registrare i dati 
+
+button.addEventListener('click', function() {
+
+    evenOddValue = evenOdd.value;
+    userNumberValue = userNumber.value;
+
+
+
+
+})
